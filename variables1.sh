1760988665c6911691d69aecb3aeecd450632675
karthik <mayarakarthik299@gmail.com>
2025-08-01 10:42:00 +0530

    added new file for password

--- variables1.sh
+++ variables1.sh
@@ -2,4 +2,6 @@
 
 echo enter username #text entered here will be added as value to variable
 
-#read username # here username is the variable
+read username # here username is the variable
+
+echo entered username is $username
